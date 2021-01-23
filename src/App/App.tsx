ec122d36f963e2fa02085f01021f72124a38f7a5
albin-woto <albin@wotoszyn.com>
2021-01-23 18:22:39 -0300

    Integrate Button component into App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,17 @@
 import * as React from "react";
 
+import Button from "../ui/controls/Button";
+
 import styles from "./App.module.scss";
 
 const App: React.FC = () => {
   return (
     <main className={styles.container}>
       <header className={styles.header}>
-        <h1>
-          <img alt="TradeHelm" src="/logo.svg" />
-        </h1>
-        <h3>Lets get this party started</h3>
+        <h1>Supermarket List</h1>
+        <h3>0 item(s)</h3>
       </header>
+      <Button colorScheme="primary">Add Item</Button>
     </main>
   );
 };
