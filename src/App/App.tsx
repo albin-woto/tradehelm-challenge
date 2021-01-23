b3809c132bb58d0c076db5fa773a3f3e0d822e24
albin-woto <albin@wotoszyn.com>
2021-01-23 20:13:52 -0300

    Add list of items from api and manage its states with hooks

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,17 +1,47 @@
 import * as React from "react";
 
+import api from "../Item/api";
+import {Item} from "../Item/types";
 import Button from "../ui/controls/Button";
 
 import styles from "./App.module.scss";
 
+enum Status {
+  Init = "init",
+  Success = "Success",
+}
+
 const App: React.FC = () => {
+  const [items, setItems] = React.useState<Item[]>([]);
+  const [status, setStatus] = React.useState<Status>(Status.Init);
+
+  React.useEffect(() => {
+    api.list().then((items) => {
+      setItems(items);
+      setStatus(Status.Success);
+    });
+  }, []);
+
+  if (status === Status.Init) {
+    return <span>Loading...</span>;
+  }
+
   return (
     <main className={styles.container}>
       <header className={styles.header}>
         <h1>Supermarket List</h1>
-        <h3>0 item(s)</h3>
+        <h3>{items.length} item(s)</h3>
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>
+              <span>{item.text}</span>
+              <button>Delete</button>
+            </li>
+          ))}
+        </ul>
       </header>
       <Button colorScheme="primary">Add Item</Button>
+      <Button colorScheme="delete">Remove All</Button>
     </main>
   );
 };
