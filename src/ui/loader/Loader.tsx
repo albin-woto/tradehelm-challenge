c0e0c52998a8ea3e7d9596f8295599ef935e4814
albin-woto <albin@wotoszyn.com>
2021-01-25 00:25:16 -0300

    Create component Loader

---
+++ src/ui/loader/Loader.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styles from "./Loader.module.scss";
+
+const Loader: React.FC = () => (
+  <div className={styles.loader}>
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default Loader;
