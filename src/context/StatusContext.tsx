feb4c3c2e041b4cbd09cfb9e62e0813498569103
albin-woto <albin@wotoszyn.com>
2021-01-25 12:48:31 -0300

    Separate Props type for StatusContext

--- src/context/StatusContext.tsx
+++ src/context/StatusContext.tsx
@@ -21,9 +21,13 @@ const InitialState = {
 
 const StatusContext = createContext<TStatusContext>(InitialState);
 
+type Props = {
+  children: React.ReactNode;
+};
+
 // I know that the return type probably shouldn't be JSX.Element, but I don't find any documentation about it
 // The es-lint rules ask for an explicit return type
-export function StatusProvider({children}: {children: React.ReactNode}): JSX.Element {
+export function StatusProvider({children}: Props): JSX.Element {
   const [status, setStatus] = useState<Status>(Status.Init);
 
   return <StatusContext.Provider value={{status, setStatus}}>{children}</StatusContext.Provider>;
