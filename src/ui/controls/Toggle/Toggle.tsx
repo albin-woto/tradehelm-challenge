6dd58cd0508049b8d75f67aa6eac172c94d9af22
albin-woto <albin@wotoszyn.com>
2021-01-25 18:12:36 -0300

    Add new Props to Toggle, fix css

--- src/ui/controls/Toggle/Toggle.tsx
+++ src/ui/controls/Toggle/Toggle.tsx
@@ -2,13 +2,20 @@ import React from "react";
 
 import styles from "./Toggle.module.scss";
 
-const Toggle: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = () => (
-  <div className={styles.toggle} id={styles.toggle}>
-    <input className={styles.toggle__input} id="toggle" type="checkbox" />
-    <label aria-label="Toggle" className={styles.toggle__label} htmlFor="toggle">
-      <div className={styles.toggle__ball} />
-    </label>
-  </div>
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  htmlFor: string;
+  onToggle: VoidFunction;
+}
+
+const Toggle: React.FC<Props> = ({id, onToggle, htmlFor}) => (
+  <>
+    <div className={styles.toggle}>
+      <input className={styles.toggle__input} id={id} type="checkbox" onClick={onToggle} />
+      <label aria-label="Toggle" className={styles.toggle__label} htmlFor={htmlFor}>
+        <div className={styles.toggle__ball} />
+      </label>
+    </div>
+  </>
 );
 
 export default Toggle;
