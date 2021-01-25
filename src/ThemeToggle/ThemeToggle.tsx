7babe6c2813a4fc176aba35c1f4a02f23d9c52a2
albin-woto <albin@wotoszyn.com>
2021-01-25 18:17:06 -0300

    Create ThemeToggle using Toggle component

---
+++ src/ThemeToggle/ThemeToggle.tsx
@@ -0,0 +1,34 @@
+import React, {useEffect, useContext} from "react";
+
+import Toggle from "../ui/controls/Toggle";
+import ThemeContext, {ThemeContextType} from "../context/ThemeContext";
+
+const ThemeToggle: React.FC = () => {
+  const {theme, setTheme} = useContext<ThemeContextType>(ThemeContext);
+
+  const updateToggleSide = (toggleCheck: HTMLInputElement) => {
+    if (theme === "dark") {
+      toggleCheck.checked = true;
+    } else if (theme === "light") {
+      toggleCheck.checked = false;
+    }
+  };
+
+  useEffect(() => {
+    const toggleCheck = document.getElementById("dark-mode-toggle") as HTMLInputElement;
+
+    updateToggleSide(toggleCheck);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [theme]);
+
+  return (
+    <Toggle
+      aria-label="Toggle input for dark-mode"
+      htmlFor="dark-mode-toggle"
+      id="dark-mode-toggle"
+      onToggle={() => setTheme(theme === "dark" ? "light" : "dark")}
+    />
+  );
+};
+
+export default ThemeToggle;
